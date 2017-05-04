1457ea14d9bfed287b7a0186b1dcc308c2bb2323
Rolf Pedro Ernst <pedrito_elcabra@hotmail.com>
2017-05-04 12:59:02 +0100

    Update CTransportTask.h

--- include/CTransportTask.h
+++ include/CTransportTask.h
@@ -2,17 +2,39 @@
 #ifndef CTRANSPORTTASK_H
 #define CTRANSPORTTASK_H
 
+#include <CBuilding.h>
+#include <CUnit.h>
+
+class CUnit;
+class CBuilding;
+
+typedef std::weak_ptr<CUnit> unit_weak_ptr;
+typedef std::weak_ptr<CBuilding> build_weak_ptr;
+
 class CTransportTask
 {
 
     public:
 
-        CTransportTask(){};
-        virtual ~CTransportTask();
-  
-    protected:
+        CTransportTask(build_weak_ptr pickUp_, build_weak_ptr dropOff_, int resource_ ){
+            pickUp = pickUp_;
+            dropOff = dropOff_;
+            resource = resource_;
+        };
+        virtual ~CTransportTask(){};
+        
+        void                AssignPorter(unit_weak_ptr porter_){ porter = porter_; };
+        bool                PorterIsAlive() { return(auto s = porter.lock() ); }
+        build_weak_ptr      GetPickUp() { return pickUp ;};
+        build_weak_ptr      GetDropOff() { return dropOff ;};
+        unit_weak_ptr       GetPorter() { return porter ;};
 
     private:
+        
+        int                 resource;
+        build_weak_ptr      pickUp;
+        build_weak_ptr      dropOff;
+        unit_weak_ptr       porter;
         
 };
 
