d0917c38b72326852d088079bd4bb7034f4a2ae1
Rolf Pedro Ernst <pedrito_elcabra@hotmail.com>
2017-05-04 12:31:18 +0100

    Update CTransportTask.h

--- include/CTransportTask.h
+++ include/CTransportTask.h
@@ -1,1 +1,19 @@
 
+#ifndef CTRANSPORTTASK_H
+#define CTRANSPORTTASK_H
+
+class CTransportTask
+{
+
+    public:
+
+        CTransportTask(){};
+        virtual ~CTransportTask();
+  
+    protected:
+
+    private:
+        
+};
+
+#endif // CTRANSPORTTASK_H
