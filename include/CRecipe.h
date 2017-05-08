be5e60dfc4429b13fcda0dd07bca681c717ec0b7
Rolf Pedro Ernst <pedrito_elcabra@hotmail.com>
2017-05-08 12:00:38 +0100

    Update CRecipe.h

--- include/CRecipe.h
+++ include/CRecipe.h
@@ -14,6 +14,8 @@ class CRecipe
         virtual ~CRecipe();
 
         void                                LoadLine(std::string key, std::string value);
+        std::map< int, int >*               GetInput(){             return &Input;};
+        std::map< int, int >*               GetOutput(){            return &Output;};
     protected:
 
     private:
