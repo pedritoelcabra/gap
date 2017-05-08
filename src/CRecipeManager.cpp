6be0e38cc2d2dda9ba2f747414b313b0d2b503fa
Rolf Pedro Ernst <pedrito_elcabra@hotmail.com>
2017-05-08 11:01:53 +0100

    Create CRecipeManager.cpp

---
+++ src/CRecipeManager.cpp
@@ -0,0 +1,45 @@
+#include "CRecipeManager.h"
+#include "CGame.h"
+
+extern CGame GAP;
+
+bool CRecipeManager::Init(){
+    CLog::Write("Loading Recipes");
+    DIR* dir = opendir("defines/recipes");
+    if (!dir){
+        CLog::Write("Couldnt open recipe directory");
+        return false;
+    }
+
+    struct dirent* entry;
+    std::string fileName = "";
+    while( (entry = readdir(dir)) != NULL ){
+        if(entry->d_namlen < 3) continue;
+        fileName = "defines/recipes/";
+        fileName.append(entry->d_name);
+        LoadRecipe(fileName);
+    }
+    return true;
+}
+
+void CRecipeManager::LoadRecipe(std::string fileName){
+  CLog::Write(fileName);
+
+	CRecipe recipe = CRecipe ();
+	std::ifstream is_file(fileName);
+	std::string line = "";
+
+    if (is_file.is_open()){
+        while( std::getline(is_file, line) ) {
+            std::istringstream is_line(line);
+            std::string key = "";
+            std::string value = "";
+            std::getline(is_line, key, '=');
+            std::getline(is_line, value);
+            recipe.LoadLine(key, value);
+        }
+	}
+
+	Recipes.push_back(recipe);
+}
+
