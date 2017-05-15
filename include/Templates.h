fa23f7aac5e38ed5aa78025a036b003af578e871
Rolf Pedro Ernst <pedrito_elcabra@hotmail.com>
2017-05-15 12:39:49 +0100

    Update Templates.h

--- include/Templates.h
+++ include/Templates.h
@@ -11,3 +11,16 @@ void DeleteUnusedWeak(const Container& cont) {
     }
 }
 
+template <class Container>
+void DeleteById(const Container& cont, int id) {
+    auto iter = cont->begin();
+    while (iter != cont->end()){
+        if(auto s = (*iter).lock()){
+            if(s->GetId() == id){
+                cont->erase(iter);
+                return;
+            }
+        }
+        iter++;    
+    }
+}
