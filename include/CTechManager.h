6c2fc5bf156b4d66c7f3ea28e90afe3ea0b4bf9b
Rolf Pedro Ernst <pedrito_elcabra@hotmail.com>
2017-05-16 16:52:58 +0100

    Update CTechManager.h

--- include/CTechManager.h
+++ include/CTechManager.h
@@ -12,15 +12,17 @@ class CTechManager
         virtual ~CTechManager(){};
 
         bool                                Init();
-        Tech*                               GetTechByName(const std::string * name);
-        bool                                IsAvailable(int tech);
-        bool                                IsResearched(int tech);
+        Tech*                               GetTechByName(const std::string * name){    return Techs.at(TechsByName.at(*name)); };
+        bool                                IsAvailable(int tech){                      return Techs.at(tech).IsResearched(); };
+        bool                                IsResearched(int tech){                     return Techs.at(tech).IsAvailable(); };
         bool                                AddProgress(int tech);
     protected:
 
     private:
+        int                                 techCounter = 0;
         void                                LoadTech(std::string fileName);
-        std::map<std::string, CTech>        Techs;
+        std::map<int, CTech>                Techs;
+        std::map<std::string, int>          TechsByName;
 };
 
 #endif // CTECHMANAGER_H
