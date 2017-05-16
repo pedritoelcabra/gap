e82920bae809b2d003149cfac00cb70152d95c99
Rolf Pedro Ernst <pedrito_elcabra@hotmail.com>
2017-05-16 15:35:07 +0100

    Create CTechManager.cpp

---
+++ src/CTechManager.cpp
@@ -0,0 +1,48 @@
+#include "CTechManager.h"
+#include "CGame.h"
+
+extern CGame GAP;
+
+bool CTechManager::Init(){
+    CLog::Write("Loading Techs");
+    DIR* dir = opendir("defines/techs");
+    if (!dir){
+        CLog::Write("Couldnt open tech directory");
+        return false;
+    }
+
+    struct dirent* entry;
+    std::string fileName = "";
+    while( (entry = readdir(dir)) != NULL ){
+        if(entry->d_namlen < 3) continue;
+        fileName = "defines/techs/";
+        fileName.append(entry->d_name);
+        LoadTech(fileName);
+    }
+    return true;
+}
+
+void CTechManager::LoadTech(std::string fileName){
+  CLog::Write(fileName);
+
+	CTech tech = CTech ();
+	std::ifstream is_file(fileName);
+	std::string line = "";
+
+    if (is_file.is_open()){
+        while( std::getline(is_file, line) ) {
+            std::istringstream is_line(line);
+            std::string key = "";
+            std::string value = "";
+            std::getline(is_line, key, '=');
+            std::getline(is_line, value);
+            recipe.LoadLine(key, value);
+        }
+	}
+
+	Techs[*(tech.GetName())] = tech;
+}
+
+CTech CTechManager::GetTechByName(std::string name){
+	return Techs.at(name);
+}
