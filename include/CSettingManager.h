8f9c171b4d339ce8b2f485fdec67124a95d017db
Rolf Pedro Ernst <pedrito_elcabra@hotmail.com>
2017-05-24 07:48:33 +0100

    Update CSettingManager.h

--- include/CSettingManager.h
+++ include/CSettingManager.h
@@ -11,7 +11,7 @@
 class CSettingManager
 {
     public:
-        enum ESettings { ChunkGenerationDistance };
+        enum ESettings { ChunkGenerationDistance, BuildingInventoryUseCooldown };
 
         CSettingManager(){};
         virtual ~CSettingManager(){};
