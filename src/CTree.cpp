ebcfd41c243fce3ac979476fccc5eec03379f366
Rolf Pedro Ernst <pedrito_elcabra@hotmail.com>
2017-05-24 14:24:50 +0100

    Update CTree.cpp

--- src/CTree.cpp
+++ src/CTree.cpp
@@ -33,21 +33,47 @@ void CTree::Set(int x_, int y_, int type_, int variety_){
         clip.y = 128;
         break;
     }
+    box.w = CScreen::tileWidth;
+    box.h = CScreen::tileWidth;
+    box.x = x_;
+    box.y = y_;
     switch(type_){
-    case 1:
-        textureName = "tree";
-        box.w = 96;
-        box.h = 96;
-        box.x = x_ - CScreen::tileWidth;
-        box.y = y_ - (CScreen::tileWidth * 2);
-        break;
-    case 2:
-        textureName = "rock";
-        box.w = 32;
-        box.h = 32;
-        box.x = x_;
-        box.y = y_;
-        break;
+        case CGood::wood:
+            textureName = "tree";
+            box.w = 96;
+            box.h = 96;
+            box.x = x_ - CScreen::tileWidth;
+            box.y = y_ - (CScreen::tileWidth * 2);
+            break;
+        case CGood::stone:
+            textureName = "rock";
+        case CGood::coal:
+            textureName = "coal";
+        case CGood::tinOre:
+            textureName = "tin";
+        case CGood::copperOre:
+            textureName = "copper";
+        case CGood::ironOre:
+            textureName = "iron";
+        case CGood::silverOre:
+            textureName = "silver";
+        case CGood::goldOre:
+            textureName = "gold";
+        case CGood::lapis:
+            textureName = "lapis";
+        case CGood::gemstones:
+            textureName = "gems";
+        case CGood::ebony:
+            textureName = "ebony";
+            box.w = 96;
+            box.h = 96;
+            box.x = x_ - CScreen::tileWidth;
+            box.y = y_ - (CScreen::tileWidth * 2);
+        case CGood::lime:
+            textureName = "lime";
+        case CGood::marble:
+            textureName = "marble";
+            break;
     }
 }
 
