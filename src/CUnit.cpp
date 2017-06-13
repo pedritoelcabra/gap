8e7b6940727beab2c2a6ef5daca7e87803e628be
Rolf Pedro Ernst <pedrito_elcabra@hotmail.com>
2017-06-13 07:38:01 +0100

    Create CUnit.cpp

--- src/CUnit.cpp
+++ src/CUnit.cpp
@@ -22,6 +22,23 @@ CUnit::CUnit(int x_, int y_, std::string myName){
         Inventory[p.first] = 0;
     }
     this->ResetFrames();
+	
+    std::string fileName = "defines/units/";
+    fileName.append(myName);
+    fileName.append(".txt");
+	std::ifstream is_file(fileName);
+	std::string line = "";
+
+    if (is_file.is_open()){
+        while( std::getline(is_file, line) ) {
+            std::istringstream is_line(line);
+            std::string key = "";
+            std::string value = "";
+            std::getline(is_line, key, '=');
+            std::getline(is_line, value);
+            LoadLine(key, value);
+        }
+	}
 }
 
 bool CUnit::PreRender(){
